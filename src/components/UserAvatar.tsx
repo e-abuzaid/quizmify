dfb34e2fff0e72f7859cdf1a24f824b43878b1ce
unknown <abu.eyaaaad@gmail.com>
2023-08-23 16:29:39 +0300

    implemented auth and added theme toggle

---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,31 @@
+import { User } from "next-auth";
+import Image from "next/image";
+import React from "react";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+
+type Props = {
+  user: Pick<User, "name" | "image">;
+};
+
+const UserAvatar = ({ user }: Props) => {
+  return (
+    <Avatar>
+      {user.image ? (
+        <div className="relative w-full h-full aspect-square">
+          <Image
+            fill
+            src={user.image}
+            alt="profile"
+            referrerPolicy="no-referrer"
+          />
+        </div>
+      ) : (
+        <AvatarFallback>
+          <span className="sr-onle">{user.name}</span>
+        </AvatarFallback>
+      )}
+    </Avatar>
+  );
+};
+
+export default UserAvatar;
